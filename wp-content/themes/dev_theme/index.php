62f96590aa17aae3933c2ad3a1f3d4610d7d2399
Thomas Bennett <admin@tgbeazy.local>
2011-06-12 12:56:24 -0500

    final slides

--- wp-content/themes/dev_theme/index.php
+++ wp-content/themes/dev_theme/index.php
@@ -1,6 +1,10 @@
 <?php ob_start(); ?>
 
 <div id="slideshow">
+    <a href="/weddings"><section class="one"></section></a>
+    <a href="/events"><section class="two"></section></a>
+    <a href="/events"><section class="three"></section></a>
+    <?php /*
     <?php $page = get_page_by_title('Main slideshow'); ?>
     <section>
         <h1><?php echo get('heading',1,1,3,$page->ID) ?></h1>
@@ -17,6 +21,7 @@
         <h2><?php echo get('content3',1,1,3,$page->ID) ?></h2>
         <?php echo get_image('slideshow_image_image2',1,1,3,$page->ID); ?>
     </section>
+    */ ?>
 </div>
 
 <section id="twitter"><img src="<?php bloginfo('template_directory') ?>/images/twitter-lg.png" alt="Twitter" class="left" /></section>
